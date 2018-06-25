a3084eb43d7f123eb77d6580473c98e93f24681d
daborcsi <36795096+daborcsi@users.noreply.github.com>
2018-06-25 23:28:50 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -39,20 +39,26 @@ function shuffle(array) {
  * Setup the cards and the game
  */
 function init() {
-  
+
   for(let i = 0; i < icons.length; i++) {
+
     const card = document.createElement("li");
     card.classList.add("card");
     card.innerHTML = `<i class="${icons[i]}"></i>`;
     cardsContainer.appendChild(card);
 
-    card = shuffle(card);
+    icons = shuffle(icons);
+    
 
     //Add click event to each card
     click(card);
+    
+
 
     }
-}    
+}   
+
+
 
 //Timer Function
 function timer() {
